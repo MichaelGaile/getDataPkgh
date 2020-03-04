ad7ba55f6f126e2b8f55e8688596d614bd688ecd
MiishaG <babusha952@gmail.com>
2020-03-04 23:29:56 +0300

    lol/ i didn't do it

--- src/generateId.js
+++
@@ -1,7 +0,0 @@
-const translit = require('cyrillic-to-translit-js');
-
-function generateId(str) {
-  return translit().transform((Array.from(str).filter((s) => /^([a-zа-яё]+|\d+)$/i.test(s))).join(''));
-}
-
-module.exports = generateId;
