15b36740934298219800d7895d66bf70e38b6038
Michael <babusha9522gmail.com>
2020-04-12 12:12:02 +0300

    Add exports Format ¯_(ツ)_/¯

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const DataPkgh = require('./DataPkgh.js');
+const Format = require('./Format.js');
 const generateId = require('./generateId.js');
 
 module.exports = DataPkgh;
 module.exports.generateId = generateId;
+module.exports.Format = Format;
